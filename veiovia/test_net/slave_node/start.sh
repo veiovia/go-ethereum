93d55271da3759b13825f805537de9ed02ddb239
Andrei S <2158075+PutPixel@users.noreply.github.com>
2021-04-08 16:34:18 +0300

    Test net setup

--- veiovia/test_net/slave_node/start.sh
+++ veiovia/test_net/slave_node/start.sh
@@ -14,7 +14,7 @@ GASPRICE=1
 PORT=6758
 ACCOUNT='0xadcc655c25fb90c4afb95a93e1b4ae76325bc7f0'
 PASSWORD_FILE_PATH='$DATADIR/pass.txt'
-BOOTNODE_ENODE='enode://012de7b075bed5bbe2aa83f51a288862795bd835b9c6a3e3ae4ff405a5f3007a782a8d7099d20e20db190091b62fa3981bfe61c5a3293e46602a6c0b7350d564@144.32.196.130:0?discport=30301'
+BOOTNODE_ENODE='enode://78e07bee752acc24002d3bd70cc398a369ef845fba7b17955a79b34fb7dfce42c1f09f459d11fa97a35a7f6e25fbb10bb754143755e9ad659c03ad310b824791@144.32.196.130:0?discport=30301'
 
 $GETH --datadir "$DATADIR" init "$DATADIR/genesis.json"
 $GETH --datadir "$DATADIR" --miner.etherbase "$ACCOUNT" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --mine --port $PORT --bootnodes '$BOOTNODE_ENODE' --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock --miner.gastarget '10000000000'
