029d5c1a4aa0923a772090d38920f4ceb43eae86
Andrei S <2158075+PutPixel@users.noreply.github.com>
2021-04-24 16:42:12 +0300

    Make bootnode public

--- veiovia/test_net/main_node/start.sh
+++ veiovia/test_net/main_node/start.sh
@@ -14,7 +14,7 @@ GASPRICE=1
 PORT=6758
 ACCOUNT='0xB335672F896e1a933948800c0DFEe3ECFceF550E'
 PASSWORD_FILE_PATH="$DATADIR/pass.txt"
-BOOTNODE_ENODE='enode://8a9bdc98654702bb74eeede99369bd449575a2d3477803847c5ad358bdcb2cad42c57e6de1b565c6c5018c7240f8be79e4e9bff8af8f31c7f43cbbd0da524fee@18.132.98.185:6758'
+BOOTNODE_ENODE='enode://78e07bee752acc24002d3bd70cc398a369ef845fba7b17955a79b34fb7dfce42c1f09f459d11fa97a35a7f6e25fbb10bb754143755e9ad659c03ad310b824791@3.9.175.11:0?discport=30301'
 
 $GETH --datadir "$DATADIR" init "$DATADIR/genesis.json"
-$GETH --datadir "$DATADIR" --miner.etherbase "$ACCOUNT" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --mine --bootnodes "$BOOTNODE_ENODE" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --miner.gastarget '10000000000'
+$GETH --datadir "$DATADIR" --nodiscover --miner.etherbase "$ACCOUNT" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --mine --bootnodes "$BOOTNODE_ENODE" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --miner.gastarget '10000000000'
--- veiovia/test_net/slave_node/start.sh
+++ veiovia/test_net/slave_node/start.sh
@@ -14,7 +14,7 @@ GASPRICE=1
 PORT=6758
 ACCOUNT='0xadcc655c25fb90c4afb95a93e1b4ae76325bc7f0'
 PASSWORD_FILE_PATH="$DATADIR/pass.txt"
-BOOTNODE_ENODE='enode://78e07bee752acc24002d3bd70cc398a369ef845fba7b17955a79b34fb7dfce42c1f09f459d11fa97a35a7f6e25fbb10bb754143755e9ad659c03ad310b824791@3.9.139.189:0?discport=30301'
+BOOTNODE_ENODE='enode://78e07bee752acc24002d3bd70cc398a369ef845fba7b17955a79b34fb7dfce42c1f09f459d11fa97a35a7f6e25fbb10bb754143755e9ad659c03ad310b824791@3.9.175.11:0?discport=30301'
 
 $GETH --datadir "$DATADIR" init "$DATADIR/genesis.json"
-$GETH --datadir "$DATADIR" --nat extip:18.132.98.185 --miner.etherbase "$ACCOUNT" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --port $PORT --bootnodes "$BOOTNODE_ENODE" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock --miner.gastarget '10000000000'
+$GETH --datadir "$DATADIR" --nat extip:18.132.98.185 --nodiscover --miner.etherbase "$ACCOUNT" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --port $PORT --bootnodes "$BOOTNODE_ENODE" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock --miner.gastarget '10000000000'
