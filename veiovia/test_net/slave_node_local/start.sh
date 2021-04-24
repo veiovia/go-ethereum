d85b5f156647c6225f052e036af54975b3524326
Andrei S <2158075+PutPixel@users.noreply.github.com>
2021-04-24 17:30:53 +0300

    Local node to test network

--- veiovia/test_net/slave_node_local/start.sh
+++ veiovia/test_net/slave_node_local/start.sh
@@ -16,4 +16,4 @@ PASSWORD_FILE_PATH="$DATADIR/pass.txt"
 BOOTNODE_ENODE='enode://78e07bee752acc24002d3bd70cc398a369ef845fba7b17955a79b34fb7dfce42c1f09f459d11fa97a35a7f6e25fbb10bb754143755e9ad659c03ad310b824791@3.9.175.11:0?discport=30301'
 
 $GETH --datadir "$DATADIR" init "$DATADIR/genesis.json"
-$GETH --datadir "$DATADIR" --nat extip:18.132.98.185 --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --bootnodes "$BOOTNODE_ENODE" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock
+$GETH --datadir "$DATADIR" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --bootnodes "$BOOTNODE_ENODE" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock
