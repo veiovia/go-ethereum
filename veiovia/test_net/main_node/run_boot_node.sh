53bb89a852f70b7a5b2fa2a90c698c7ba84c9b07
Andrei S <2158075+PutPixel@users.noreply.github.com>
2021-04-28 12:09:00 +0300

    Don't expose sealer

--- veiovia/test_net/main_node/run_boot_node.sh
+++ veiovia/test_net/main_node/run_boot_node.sh
@@ -8,4 +8,5 @@ DATADIR='./datadir'
 #Chain setup
 BOOT_NODE_KEY_NAME='test-veiovia'
 
-$BOOTNODE --nodekey "$BOOT_NODE_KEY_NAME" --addr '172.31.19.209:30301'
+#$BOOTNODE --nodekey "$BOOT_NODE_KEY_NAME" --verbosity 6 --addr '172.31.19.209:30301'
+$BOOTNODE --nodekey "$BOOT_NODE_KEY_NAME" --verbosity 6
