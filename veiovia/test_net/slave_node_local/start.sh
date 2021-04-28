33dc3415d470ef552d99297cb8c8e22133ea8b49
Andrei S <2158075+PutPixel@users.noreply.github.com>
2021-04-28 13:15:16 +0300

    Don't expose sealer

--- veiovia/test_net/slave_node_local/start.sh
+++ veiovia/test_net/slave_node_local/start.sh
@@ -13,6 +13,7 @@ GASPRICE=1
 #Miner setup
 ACCOUNT='0xadcc655c25fb90c4afb95a93e1b4ae76325bc7f0'
 PASSWORD_FILE_PATH="$DATADIR/pass.txt"
+BOOTNODE_ENODE='enode://5b668de7caf270fa4e38ab05936deef2bdc4825cd6bf572dd4b3c3fdc14bc617265238e21178cc72f6d774ea508cb0d239ce8571694f770da9ceb60e44439b76@18.132.98.185:6758'
 
 $GETH --datadir "$DATADIR" init "$DATADIR/genesis.json"
-$GETH --datadir "$DATADIR" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock
+$GETH --datadir "$DATADIR" --bootnodes "$BOOTNODE_ENODE" --unlock "$ACCOUNT" --password "$PASSWORD_FILE_PATH" --networkid $NETWORKID --gasprice "$GASPRICE" --syncmode "full" --http --allow-insecure-unlock
